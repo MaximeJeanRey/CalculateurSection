5d0c3686750863f2670c02f86f889af9b9461d68
Maxime Rey <maximejeanrey@gmail.com>
2022-09-17 21:09:38 +0200

    Added "Chute de tension"

--- Qt/SecApp/cable.cpp
+++ Qt/SecApp/cable.cpp
@@ -5,7 +5,7 @@ cable::cable(bool phase)
     this->phase = phase;
 }
 
-float cable::searchNearest(float key)
+float cable::searchNearest(float key, float tabToSearch[])
 {
     float value = 0;
     float previousValue;
@@ -16,11 +16,31 @@ float cable::searchNearest(float key)
     }
     for(int x = 0; x<16; x++)
     {
-        value = key - this->SecTab[x];
+        value = key - tabToSearch[x];
         if(value < 0)
         {
-           return this->SecTab[x];
+           return tabToSearch[x];
         }
    }
-   return -1;
+    return -1;
+}
+
+int cable::getIndex(float element, float arr[16])
+{
+    float result= 0;
+
+    for (int i = 0; i <= 16; i++){
+        result  = arr[i]/element;
+        if (result == 1){
+            return i;
+        }
+    }
+    return 0;
+}
+
+float cable::chuteReele(float L,float i, float S, bool tri)
+{
+    float x = (2*i*L*((this->pcui/S)*this->cosPhi+this->X*(this->sinPhi)))/this->V;
+    x = x/(tri+1); // si tri = 0 alors on change pas, sinon on divise par deux
+    return x*100;
 }
--- Qt/SecApp/ongletcalcul.cpp
+++ Qt/SecApp/ongletcalcul.cpp
@@ -1,6 +1,8 @@
 #include "ongletcalcul.h"
 #include "ui_ongletcalcul.h"
 #include "cable.h"
+#include <iostream>
+#include <algorithm>
 
 cable tableauTab(0);
 
@@ -18,6 +20,11 @@ OngletCalcul::~OngletCalcul()
 
 void OngletCalcul::on_callResult_clicked()
 {
+    float FinalResult = 0;
+    float actualIz = 0;
+    float formerIz = 0;
+    int sectionIndex = 0;
+
     float pcui = 0.023;
     float palu = 0.037;
 
@@ -28,9 +35,11 @@ void OngletCalcul::on_callResult_clicked()
 
     if (ui->cType->currentIndex() == 0){
         tableauTab.p = pcui;
+        tableauTab.matiere = 0;
     }
     else {
         tableauTab.p = palu;
+        tableauTab.matiere = 1;
     }
     if (ui->cPhase->currentIndex() == 0){
         tableauTab.phase = 0;
@@ -49,7 +58,52 @@ void OngletCalcul::on_callResult_clicked()
     step2 = (tableauTab.p/step2)/(tableauTab.phase+1);
 
 
-    ui->dvResult->setValue(tableauTab.searchNearest(step2));
+    FinalResult = tableauTab.searchNearest(step2, tableauTab.SecTab);
+
+
+    sectionIndex = tableauTab.getIndex(FinalResult, tableauTab.SecTab); //Récupération de l'index de l'actuelle section calculée
+
+
+    if(tableauTab.matiere == 0 &&  tableauTab.phase == 0){ //Ctype = cuivre / alu
+        formerIz = tableauTab.izMonoCuivreTab[sectionIndex];
+        actualIz = tableauTab.searchNearest(ui->vCourant->value(), tableauTab.izMonoCuivreTab);
+        sectionIndex = tableauTab.getIndex(actualIz, tableauTab.izMonoCuivreTab);
+    }
+    else if(tableauTab.matiere == 0 &&  tableauTab.phase == 1){
+        formerIz = tableauTab.izTriCuivreTab[sectionIndex];
+        actualIz = tableauTab.searchNearest(ui->vCourant->value(), tableauTab.izTriCuivreTab);
+        sectionIndex = tableauTab.getIndex(actualIz, tableauTab.izTriCuivreTab);
+    }
+    else if(tableauTab.matiere == 1 &&  tableauTab.phase == 0){
+        formerIz = tableauTab.izMonoAluTab[sectionIndex];
+        actualIz = tableauTab.searchNearest(ui->vCourant->value(), tableauTab.izMonoAluTab);
+        sectionIndex = tableauTab.getIndex(actualIz, tableauTab.izMonoAluTab);
+    }
+    else if(tableauTab.matiere == 1 &&  tableauTab.phase == 1){
+        formerIz = tableauTab.izTriAluTab[sectionIndex];
+        actualIz = tableauTab.searchNearest(ui->vCourant->value(), tableauTab.izTriAluTab);
+        sectionIndex = tableauTab.getIndex(actualIz, tableauTab.izTriAluTab);
+    }
+    else
+    {
+        actualIz = 999999;
+        FinalResult = 999999; //simple sécurité
+    }
+
+    if (formerIz < ui->vCourant->value()){
+
+        FinalResult = tableauTab.SecTab[sectionIndex];
+    }
+
+    if(tableauTab.p == palu && FinalResult < 16){
+        FinalResult = 16;
+    }
+
+    ui->dvResult->setValue(FinalResult);
+    ui->doubleSpinBox->setValue(tableauTab.chuteReele(ui->vLongueur->value(),ui->vCourant->value(), FinalResult, tableauTab.phase));
+
+
+
 }
 
 
